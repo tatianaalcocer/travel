45c0a99fd89b251b8eef1978aab8d584f41a5d60
Banesa Guaderrama <banesa.guaderrama@hotmail.com>
2018-12-04 21:52:56 -0500

    API Routes

---
+++ public/concert.js
@@ -0,0 +1,12 @@
+const getEventList = function(e){
+    e.preventDefault();
+    $.get(`api/events/${$('#location').val()}`)
+    .then( function(data){
+      for (let i = 0; i < 10; i++){
+        $('#event').append(`<div class="response">${data[i].name}<div>`)
+      }
+    }
+    )
+  }
+  
+  $('#submit').on('click', getEventList);
--- routes/concert-api-routes.js
+++ routes/concert-api-routes.js
@@ -1,3 +1,27 @@
-module.exports = function (app) {
+const $ = require('axios');
+require('dotenv').config()
 
+module.exports = function (app) {
+    console.log("api-route")
+    app.get('api/events/:location', function(req,res){
+        console.log("city")
+        $.get(`https://app.ticketmaster.com/discovery/v2/events.json?city=US${req.params.location}&apikey=${process.env.TICKET_API}`)
+        // $.get(`https://app.ticketmaster.eu/mfxapi/v1/events.json?city=US${req.params.location}&apikey=${process.env.TICKET_API}`)
+        .then(function(response){
+            res.json(response.data);
+          })
+    })
 }
+
+//Example
+// const $ = require('axios');
+// require('dotenv').config()
+
+// module.exports = function (app) {
+//   app.get('FILL IN THE BLANK HERE WITH YOUR ROUTE AND PARAMETER', function(req,res){
+//     $.get(`https://myqueryURL/stuffgoeshere/get-this-from-documentation&location=${req.params.location}&apikey=${process.env.TICKET_API}`)
+//     .then(function(response){
+//       res.json(response);
+//     })
+//   })
+// }
