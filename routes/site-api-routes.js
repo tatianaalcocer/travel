e430651ddc67d58bf8aaf02de1bb549fffbbc3c9
veronicorn <vronicalee@gmail.com>
2018-12-06 15:43:04 -0500

    Adds API routes for sites. Does not function correctly yet.

--- routes/site-api-routes.js
+++ routes/site-api-routes.js
@@ -1,3 +1,11 @@
-module.exports = function (app) {
+const $ = require('axios');
+require('dotenv').config()
 
+module.exports = function (app) {
+    app.get('/api/sites/:location', function (req, res) {
+        $.get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?location=${req.params.location}+points_of_interest&apikey=${process.env.PLACES_API}`)
+            .then(function (response) {
+                res.json(response.data);
+            })
+    })
 }
