924d0d615854612c1e5be261b8eb47bc6f13c396
wmahathre <wmahathre@gmail.com>
2018-12-06 15:54:17 -0500

    Required the dotenv package on server.js and process.env on the restaurant-api-routes.js line 10  to store the API key. Installed yelp=fusion package

--- routes/restaurant-api-routes.js
+++ routes/restaurant-api-routes.js
@@ -1,3 +1,23 @@
 module.exports = function (app) {
 
+
+    module.exports = function (app) {
+
+        'use strict';
+     
+        const yelp = require('yelp-fusion');
+         
+        const client = yelp.client(process.env.YELP_API);
+         
+        client.search({
+          term:'Four Barrel Coffee',
+          location: 'san francisco, ca'
+        }).then(response => {
+          console.log(response.jsonBody.businesses[0].name);
+        }).catch(e => {
+          console.log(e);
+        });
+    
+    
+    }
 }
