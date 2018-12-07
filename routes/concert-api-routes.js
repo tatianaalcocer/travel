d7b5a24093a2fab35dd6f2b4c1591aea394a5bf7
Banesa Guaderrama <banesa.guaderrama@hotmail.com>
2018-12-06 22:03:06 -0500

    Render

--- routes/concert-api-routes.js
+++ routes/concert-api-routes.js
@@ -2,12 +2,32 @@ const $ = require('axios');
 require('dotenv').config()
 
 module.exports = function (app) {
-    console.log("api-route")
+    // console.log("api-route")
     app.get('/api/events/:location', function(req,res){
-        console.log("city")
+        // console.log("city")
         $.get(`https://app.ticketmaster.com/discovery/v2/events.json?city=${req.params.location}&apikey=${process.env.TICKET_API}`)
         .then(function(response){
             res.json(response.data._embedded.events);
           })
+          .catch(function (err) {
+            res.json(err);
+        });
+
     })
 }
+
+// const $ = require(‘axios’);
+// require(‘dotenv’).config();
+
+// module.exports = function (app) {
+
+//    app.get(‘/api/sites/:location’, function (req, res) {
+//        $.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${req.params.location}+point+of+interest&language=en&key=${process.env.PLACES_API}`)
+//            .then(function (response) {
+//                res.json(response.data.results);
+//            })
+//            .catch(function (err) {
+//                res.json(err);
+//            });
+//    })
+// }
