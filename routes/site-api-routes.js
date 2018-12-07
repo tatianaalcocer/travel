36463b5c16823d02a081e2e62f664253c6f52536
veronicorn <vronicalee@gmail.com>
2018-12-06 21:29:02 -0500

    Successfully renders 10 site names to index.html!

--- routes/site-api-routes.js
+++ routes/site-api-routes.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
     app.get('/api/sites/:location', function (req, res) {
         $.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${req.params.location}+point+of+interest&language=en&key=${process.env.PLACES_API}`)
             .then(function (response) {
-                res.json(response.data);
+                res.json(response.data.results);
             })
             .catch(function (err) {
                 res.json(err);
