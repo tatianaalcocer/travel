5cc3746964bb019ea93f5e7743ec7b9a3096f44a
Banesa Guaderrama <banesa.guaderrama@hotmail.com>
2018-12-08 11:31:49 -0500

    Add clickable function in a separate page

--- public/concert.js
+++ public/concert.js
@@ -4,7 +4,7 @@ const getEventList = function(e){
     $.get(`/api/events/${$('#location').val()}`)
     .then( function(data){
       for (let i = 0; i < 10; i++){
-        $('#event').append(`<div class="response">${data[i].name}<div>`)
+        $('#event').append(`<div class="response"><a href='${data[i].url}'target='_blank'>${data[i].name}</a><div>`)
       }
     }
     )
