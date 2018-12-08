e16f6d5f35a19741c6324fb1fb98a26390cbcc1e
veronicorn <vronicalee@gmail.com>
2018-12-08 11:05:16 -0500

    Appends sites' addresses to page.

--- public/site.js
+++ public/site.js
@@ -5,6 +5,7 @@ const getSiteList = function (e) {
         .then(function (data) {
             for (let i = 0; i < 10; i++) {
                 $('#sites').append(`<div class="response">${data[i].name}<div>`)
+                $('#sites').append(`<div class="response-address">${data[i].formatted_address}<div>`)
             }
         })
 }
