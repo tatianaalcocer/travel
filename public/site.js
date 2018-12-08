a92ab4bbb34f43b2b9191754247cda730d724ddf
veronicorn <vronicalee@gmail.com>
2018-12-08 13:55:30 -0500

    Adds additional results to sites div.

--- public/site.js
+++ public/site.js
@@ -9,6 +9,10 @@ const getSiteList = function (e) {
                 $('#sites').append(`<div class="response">${data[i].name}<div>`)
                 $('#sites').append(`<div class="response-address">${data[i].formatted_address}<div>`)
             }
+            // if (data = false) {
+            //     const noResults = 'No results found';
+            //     noResults();
+            // }
         })
 }
 
@@ -17,7 +21,7 @@ const showMoreSites = function (e) {
     showMoreCount++;
     $.get(`api/sites/${$('#location').val()}`)
         .then(function (data) {
-            for (let i = showMoreCount * 10 + 1; i < showMoreCount * 10 + 1; i++) {
+            for (let i = showMoreCount * 10 + 1; i < showMoreCount * 10 + 11; i++) {
                 $('#sites').append(`<div class="response">${data[i].name}<div>`)
                 $('#sites').append(`<div class="response-address">${data[i].formatted_address}<div>`)
             }
