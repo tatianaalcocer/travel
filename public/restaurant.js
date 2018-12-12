934752726992d62384e89b05a1daf9d866b43154
wmahathre <wmahathre@gmail.com>
2018-12-11 21:00:19 -0500

    got the restaurant and concert.js files to render the next set of 10  after the show more button is clicked more than once

--- public/restaurant.js
+++ public/restaurant.js
@@ -1,38 +1,27 @@
 // let showMoreCount = 0;
 const getRestaurantList = function(e){
-    e.preventDefault();
-    $('#restaurant').empty();
-    $('#restaurant').append(`<div class="search-topic"><h1>Restaurant</h1><div>`)
-    $.get(`api/restaurant/${$('#location').val()}`)
-    .then( function(data){
-      for (let i = 0; i < 10; i++){
-        $('#restaurant').append(` <a target="_blank" href="${data[i].url}"<div class="response">${data[i].name}</a><div>`)
-      }
+  e.preventDefault();
+  $('#restaurant').empty();
+  $('#restaurant').append(`<div class="search-topic"><h1>Restaurant</h1><div>`)
+  $.get(`api/restaurant/${$('#location').val()}`)
+  .then( function(data){
+    for (let i = 0; i < 10; i++){
+      $('#restaurant').append(` <a target="_blank" href="${data[i].url}"<div class="response">${data[i].name}</a><div>`)
     }
-    )
   }
-
-  $('#submit').on('click', getRestaurantList);
-
-
-
-  const showMore = function(e){
-    e.preventDefault();
-    // showMoreCount++
-    $.get(`api/restaurant/${$('#location').val()}`)
-    .then( function(data){
-        for (let i = showMoreCount * 10 + 1; i < showMoreCount * 10 + 11; i++) {
-     
-        $('#restaurant').append(` <a target="_blank" href="${data[i].url}"<div class="response">${data[i].name}</a><div>`)
-      }
+  )
+}
+$('#submit').on('click', getRestaurantList);
+const showMore = function(e){
+  e.preventDefault();
+  // showMoreCount++
+  $.get(`api/restaurant/${$('#location').val()}`)
+  .then( function(data){
+      for (let i = 0; i < data.length; i++){
+   
+      $('#restaurant').append(` <a target="_blank" href="${data[i].url}"<div class="response">${data[i].name}</a><div>`)
     }
-    )
   }
-
-  $('#btn-showMore').on('click', showMore);
-
-
-//   showMore = function() {
-//       showMoreCount++;
-//       for (i = (showMoreCount * 10) + 1; i < (showMoreCount * 10) + 1; i++
-//   }
+  )
+}
+$('#btn-showMore').on('click', showMore);
--- routes/restaurant-api-routes.js
+++ routes/restaurant-api-routes.js
@@ -1,24 +1,30 @@
-'use strict';
 require('dotenv').config()
-
-        const yelp = require('yelp-fusion');
+const yelp = require('yelp-fusion');
          
-        const client = yelp.client(`${process.env.YELP_API}`);
+let cachedLocation = ''
+let currentOffset = 0
+         const client = yelp.client(`${process.env.YELP_API}`);
         
          module.exports = function (app) {
-
+         
         
-        app.get('/api/restaurant/:location', function (req, res) {
-
+         app.get('/api/restaurant/:location/', function (req, res) {
+            if (cachedLocation != req.params.location) {
+                currentOffset = 0;
+            }
             client.search({ 
             term:'food',
-            location: req.params.location
+            location: req.params.location,
+            limit: 10,
+            offset: currentOffset * 10
             }).then(response => {
             res.json(response.jsonBody.businesses);
             }).catch(e => {
             res.json(e);
             });
     
+            currentOffset++;
+            cachedLocation = req.params.location;
     });
     
-}
+} 
