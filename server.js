aba373f4a0ccaad0dde43b58abb5a62b144b3b1c
veronicorn <vronicalee@gmail.com>
2018-12-11 19:21:01 -0500

    Removes empty Hotel files and mention of mongoose in server.js. Also removes commented out code.

--- server.js
+++ server.js
@@ -6,21 +6,14 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-
-// mongoose.connect('mongodb://localhost/twitter', { useNewUrlParser: true });
-
-
 require('./routes/concert-api-routes')(app);
-require('./routes/hotel-api-routes')(app);
 require('./routes/restaurant-api-routes')(app);
 require('./routes/site-api-routes')(app);
 require('./routes/html-routes')(app);
-
 
 app.listen(PORT, function() {
   console.log(`App running on port ${PORT}`);
